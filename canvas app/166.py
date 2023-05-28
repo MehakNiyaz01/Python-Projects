efac2ce164e8052085b2a8c0b09a4b3dad2aaf10
MehakNiyaz01 <131400307+MehakNiyaz01@users.noreply.github.com>
2023-05-28 17:32:26 +0530

    Add files via upload

---
+++ canvas app/166.py
@@ -0,0 +1,116 @@
+
+from tkinter import *
+from PIL import ImageTk, Image
+
+root= Tk()
+root.title("Working On Canvas Using Functions")
+root.geometry("600x600")
+
+
+color_label=Label(root, text="Enter a Color :")
+color_label.place(relx=0.6,rely=0.9, anchor= CENTER)
+
+input_box = Entry(root)
+input_box.insert(0,"black")
+input_box.place(relx=0.8,rely=0.9, anchor= CENTER)
+
+
+
+canvas=Canvas(root, width = 590, height=510, bg = "white", highlightbackground="lightgray")
+canvas.pack()
+
+
+img =ImageTk.PhotoImage(Image.open ("start_point1.png"))
+my_image = canvas.create_image(50,50,image=img)
+
+direction = ""
+oldx=50
+oldy=50
+newx=50
+newy=50
+
+def draw( direction,oldx,oldy, newx ,newy):
+    fill_color = input_box.get()
+    
+    if(direction=="right"):
+       right_line=canvas.create_line(oldx,oldy,newx,newy,width=3,fill=fill_color)
+    if(direction=="left"):
+       left_line= canvas.create_line(oldx,oldy,newx,newy,width=3,fill=fill_color)
+    if(direction=="up"):
+       up_line= canvas.create_line(oldx,oldy,newx,newy,width=3,fill=fill_color)
+    if(direction=="down"):
+       down_line= canvas.create_line(oldx,oldy,newx,newy,width=3,fill=fill_color)
+
+
+
+
+
+
+
+def right_dir(e):
+    global direction
+    global oldx
+    global oldy
+    global newx
+    global newy
+    oldx=newx
+    oldy=newy
+    newx=newx+5
+    newy=newy
+    direction="right"
+    draw(direction,oldx,oldy,newx,newy)
+    
+def left_dir(e):
+        global direction
+        global oldx
+        global oldy
+        global newx
+        global newy
+        oldx=newx
+        oldy=newy
+        newx=newx-5
+        newy=newy
+        direction="left"
+        draw(direction,oldx,oldy,newx,newy)
+         
+def up_dir(e):
+    global direction
+    global oldx
+    global oldy
+    global newx
+    global newy
+    oldx=newx
+    oldy=newy
+    new=newx
+    newy=newy-5
+    direction="up"
+    draw(direction,oldx,oldy,newx,newy)
+def down_dir(e):
+    global direction
+    global oldx
+    global oldy
+    global newx
+    global newy
+    oldx=newx
+    oldy=newy
+    newx=newx
+    newy=newy+5
+    direction="down"
+    draw(direction,oldx,oldy,newx,newy)
+     
+     
+     
+  
+    
+  
+    
+  
+    
+root.bind("<Right>",right_dir)
+root.bind("<Left>",left_dir)
+root.bind("<Up>",up_dir)
+root.bind("<Down>",down_dir)
+root.mainloop()
+
+
+
